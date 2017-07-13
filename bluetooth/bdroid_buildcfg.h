bae22a4a873eea0d1d8042f11c0367f5a8f9c079
SJR (Sam) <sjrmac@gmail.com>
2017-07-12 21:55:19 -0700

    Set a name "Lumia 720" for whenever we get BT working...

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Lumia"
+#define BTM_DEF_LOCAL_NAME   "Lumia 720"
 #define BTA_DISABLE_DELAY 1000
 #define BLUETOOTH_QTI_SW TRUE
 
